975ae28bae4d086f4be0d243bab2bae62c4a0534
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-01-26 11:15:48 +0100

    S101N1e2 primera versión

--- S101/src/N1exercici1/Vehicle.java
+++ S101/src/N1exercici1/Vehicle.java
@@ -71,4 +71,6 @@ public class Vehicle {
 
    }
 
+    public void parar() {
+    }
 }
---
+++ S101/src/N1exercici2/Main.java
@@ -0,0 +1,17 @@
+package N1exercici2;
+
+public class Main {
+
+    public static void main(String[] args) {
+ /* A la classe Vehicle afegeix dos mètodes: un static anomenat parar() i un altre no-static anomenat accelerar().
+ Demostra com invocar el mètode static i el no-static des del main() (per exemple, enviant un missatge a la consola).
+  */
+        Vehicle coche1 = new Vehicle("Seat", "León", "Rojo", 2015, 4);
+
+        coche1.iniciar();
+        coche1.acelerar();
+        Vehicle.parar();
+
+    }
+}
+
