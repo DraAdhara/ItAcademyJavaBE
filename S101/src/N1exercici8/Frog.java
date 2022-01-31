ed93bd5dc8d6421b930e4771533e6cd889216cc2
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-01-31 10:04:57 +0100

    S101 n1ex8

---
+++ S101/src/N1exercici8/Frog.java
@@ -0,0 +1,21 @@
+package N1exercici8;
+
+
+    public class Frog extends Amphibian {
+@Override
+        public void mover() {
+            System.out.println("Soy una frog y me estoy moviendo.");
+        }
+@Override
+        public void respirar() {
+            System.out.println("Soy una frog y estoy respirando.");
+        }
+@Override
+        public void comer() {
+            System.out.println("Soy una frog y estoy comiendo.");
+        }
+        public void saltar() {
+            System.out.println("Estoy saltando.");
+        }
+    }
+
