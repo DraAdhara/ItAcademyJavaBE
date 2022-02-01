5aea16de48a67204e99fc825df75893e065f6789
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-02-01 17:26:09 +0100

    S103 N1 exe4rcicis 1 a 4

---
+++ S103/src/N1exercici5/Main.java
@@ -0,0 +1,12 @@
+package N1exercici5;
+
+public class Main {
+
+    public static void main (String []args){
+    /*Exercici 5. Crea i emplena un objecte List<Integer>. Crea i emplena un segon objecte List<Integer> de la mateixa
+    grandària que el primer. Utilitza sengles objectes ListIterator per a llegir els elements de la primera llista i
+    inserir-los en la segona en ordre invers (provi d'explorar diverses formes diferents de resoldre aquest problema).
+     */
+
+    }
+}
