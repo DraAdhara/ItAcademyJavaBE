c8d39de8cec25a6e8ce6c5683bdd3cd49ccb2f15
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-02-01 18:11:32 +0100

    S103 N1 exe4rcicis 4 y 5

--- S103/src/N1exercici4/Main.java
+++ S103/src/N1exercici4/Main.java
@@ -19,29 +19,33 @@ public class Main {
          */
         Objeto obj = new Objeto("nuevo");
 
-
+        //Añadimos objetos a ArrayList
         ArrayList<Objeto> listaAL = new ArrayList<Objeto>();
         listaAL.add(new Objeto("ArrayList1"));
         listaAL.add(new Objeto("ArrayList2"));
 
+        //Añadimos objetos a LinkedList
         LinkedList<Objeto> listaLL = new LinkedList<Objeto>();
         listaLL.add(new Objeto("LinkedList1"));
         listaLL.add(new Objeto("LinkedList2"));
 
+        //Añadimos objetos a HashSet
         HashSet<Objeto> listaHS = new HashSet<Objeto>();
         listaHS.add(new Objeto("HashSet1"));
         listaHS.add(new Objeto("HashSet2"));
 
+        //Añadimos objetos a LinkedHashSet
         LinkedHashSet<Objeto> listaLHS = new LinkedHashSet<Objeto>();
         listaLHS.add(new Objeto("LinkedHashSet1"));
         listaLHS.add(new Objeto("LinkedHashSet2"));
 
+        //Añadimos objetos a TreeSet
         TreeSet<Objeto> listaTS = new TreeSet<Objeto>();
         listaTS.add(new Objeto("TreeSet1"));
         listaTS.add(new Objeto("TreeSet2"));
 
 
-
+        //Recorremos las colecciones
         iterador(listaAL);
         iterador(listaLL);
         iterador(listaHS);
