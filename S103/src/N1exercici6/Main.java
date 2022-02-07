6cea7dfc98691d1062726e71763dad9cc0adb0e8
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-02-07 12:39:23 +0100

    S103 N1exercici6 correccio

--- S103/src/N1exercici6/Main.java
+++ S103/src/N1exercici6/Main.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
+    static int numIngresado;
     public static void main (String[]args){
     /*Exercici 6. Crea un objecte buit LinkedList<Integer>. Utilitzant un iterador ListIterator, afegeixi valors sencers
      a la llista inserint-los sempre a la meitat d'aquesta.
@@ -12,24 +13,27 @@ public class Main {
         LinkedList<Integer> listL = new LinkedList<Integer>();
         Iterator<Integer> listIterator = listL.iterator();
         Scanner input = new Scanner(System.in);
+        int control;
 
         do {
             listL.add(listL.size()/2, ingresaNumero());
             System.out.println(listL);
 
-        } while (listL.listIterator() != null );
+
+        } while (numIngresado != 0);
+     ///---> versión anterior con bucle infinito:while (listL.listIterator() != null );
 
     }
 
     public static int ingresaNumero() {
         Scanner input = new Scanner(System.in);
-        int numIngresado;
-        do {
-            System.out.println("Ingresa un numero entero para añadirlo a la lista, detén el programa para salir: ") ;
+        ///---> versión anterior con bucle infinito:int numIngresado;
+        ///---> versión anterior con bucle infinito:do {
+            System.out.println("Ingresa un numero entero para añadirlo a la lista, marca 0 par salir: ") ;
             numIngresado = input.nextInt();
             return numIngresado;
         }
-        while (numIngresado != 0);
+        //---> versión anterior con bucle infinito: while (numIngresado != 0);
 
     }
-}
+
