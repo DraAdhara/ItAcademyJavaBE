de17e017dc715fc9e54b08d4a8c7cba3be8b779f
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-02-09 19:38:14 +0100

    S106 1 y 2

---
+++ S106/src/N1exercici2/GenericMethod.java
@@ -0,0 +1,10 @@
+package N1exercici2;
+
+public class GenericMethod <T>{
+
+    public void f(T a1, T a2, T a3) {
+        System.out.println(a1+" "+a2+" "+a3);
+    }
+
+
+}
---
+++ S106/src/N1exercici2/Main.java
@@ -0,0 +1,20 @@
+package N1exercici2;
+
+public class Main {
+    public static void main(String[]args){
+ /*Exercici 2. Crea una classe anomenada GenericMethods amb un mètode genèric anomenat f() que accepti tres arguments
+ de tipus genèric. Realitza al main() diferents crides  d'aquest mètode amb diferents tipus de parametres.
+ Comprova que es poden posar qualsevol tipus de variable.
+  */
+        GenericMethod gm = new GenericMethod();
+
+        gm.f("Estrella", "hola", 5);
+
+        gm.f(10, "que tal", true);
+
+        gm.f(false,"adios", 3);
+
+    }
+}
+
+
