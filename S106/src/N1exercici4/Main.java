13ff75b7df75511634adf7ca309d5b66d8a6ffb9
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-02-10 09:56:25 +0100

    S106

--- S106/src/N1exercici4/Main.java
+++ S106/src/N1exercici4/Main.java
@@ -3,6 +3,8 @@ package N1exercici4;
 public class Main {
 
     public static void main (String[] args) {
+    //Exercici 4. Modifica l'anterior apartat de manera que els arguments que rep f() sigui una llista d'arguments
+        // de variable indefinida.
         GenericMethod gm = new GenericMethod();
 
         gm.f("Estrella", "Estrella", "Estrella", true);
