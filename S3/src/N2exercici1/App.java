857374cea81f6a4a925281035a4eee43b3813768
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-03-29 18:57:11 +0200

    S301 N2

--- S3/src/N2exercici1/App.java
+++ S3/src/N2exercici1/App.java
@@ -3,7 +3,12 @@ package N2exercici1;
 public class App {
 
     public static void main(String [] args){
+/*Crea un petit gestor de direccions i números de telèfon internacionals.
 
+L'aplicació permetrà afegir a l'agenda Adreces i Números de telèfon internacionals. Tenint en compte els diferents
+formats de diversos països, construeixi l'agenda i les adreces i telèfons que afegeix implementant un patró Abstract Factory.
+
+ */
         EspanaFactory espana1 = new EspanaFactory("Victor Valdés","765432189","Arístides Mallol, Barcelona");
         PortugalFactory portugal1 = new PortugalFactory("Vitor Baía","12345678","Estadio do Dragao, Porto");
 
