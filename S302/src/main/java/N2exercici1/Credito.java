7ce02950d49cdb4896b482f8ed7e1a0857fbb74c
DraAdhara <98258406+DraAdhara@users.noreply.github.com>
2022-04-06 18:11:48 +0200

    S302 N1ex2

---
+++ S302/src/main/java/N2exercici1/Credito.java
@@ -0,0 +1,9 @@
+package N2exercici1;
+
+public class Credito implements IFormaPago{
+
+    @Override
+    public void pago() {
+        System.out.println("Pago realizado con tarjeta de crédito.");
+    }
+}
--- S302/src/main/java/N2exercici1/Main.java
+++ S302/src/main/java/N2exercici1/Main.java
@@ -1,4 +1,26 @@
 package N2exercici1;
 
 public class Main {
+ public static void main (String []args){
+ /*CallBack: Simula una passarel·la de pagament que rebi un objecte encapsulador del mètode de pagament a efectuar:
+ targeta de crèdit, Paypal o deute en compte bancari.
+
+La passarel·la invocarà el pagament sense conèixer la forma i retornant el control a la classe d'origen.
+La classe que invoca la passarel·la de pagament serà una botiga de sabates.
+
+  */
+//Generamos las tiendas
+  TiendaZapatos merkal = new TiendaZapatos();
+  TiendaZapatos spartoo = new TiendaZapatos();
+
+  IFormaPago a = new Credito();
+  IFormaPago b = new PayPal();
+  IFormaPago c = new CuentaBanco();
+
+//Llamamos a los callback
+merkal.pago(a);
+merkal.pago(b);
+spartoo.pago(c);
+ }
+
 }
