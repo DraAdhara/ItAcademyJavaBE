faa8081d556880aa439a3d5e5a9bf7b99b074644
Adhara Domínguez <98258406+DraAdhara@users.noreply.github.com>
2022-05-25 17:13:21 +0200

    S04.01 N2

---
+++ S4/S04T01N02GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/controllers/HelloWorldController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class HelloWorldController {
+    @RestController
+    public class HelloWorldController {
+
+        @GetMapping("/HelloWorld")
+        public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom) {
+            return "Hola, " + nom + ". Estàs executant un projecte Gradle.";
+        }
+
+        @GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
+        public String saluda2(@PathVariable(required = false) String nom) {
+            return "Hola, " + nom + ". Estàs executant un projecte Gradle.";
+        }
+    }
+}
