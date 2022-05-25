256e9e579a562b32d0a8c1d658815d4362c15544
Adhara Domínguez <98258406+DraAdhara@users.noreply.github.com>
2022-05-25 17:17:29 +0200

    Update HelloWorldController.java

--- S4/S04T01N02GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/controllers/HelloWorldController.java
+++ S4/S04T01N02GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/controllers/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-public class HelloWorldController {
+
     @RestController
     public class HelloWorldController {
 
