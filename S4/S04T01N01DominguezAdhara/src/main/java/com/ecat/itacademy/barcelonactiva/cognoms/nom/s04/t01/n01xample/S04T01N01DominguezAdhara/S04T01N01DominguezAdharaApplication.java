ffc8bd534247c2a4cb414d1a2aa57da9808faf85
Adhara Domínguez <98258406+DraAdhara@users.noreply.github.com>
2022-05-25 11:49:44 +0200

    Add files via upload
    Initial commit

---
+++ S4/S04T01N01DominguezAdhara/src/main/java/com/ecat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01xample/S04T01N01DominguezAdhara/S04T01N01DominguezAdharaApplication.java
@@ -0,0 +1,13 @@
+package com.ecat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01xample.S04T01N01DominguezAdhara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01DominguezAdharaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01DominguezAdharaApplication.class, args);
+	}
+
+}
---
+++ S4/S04T01N01DominguezAdhara/src/test/java/com/ecat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01xample/S04T01N01DominguezAdhara/S04T01N01DominguezAdharaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01xample.S04T01N01DominguezAdhara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01DominguezAdharaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
