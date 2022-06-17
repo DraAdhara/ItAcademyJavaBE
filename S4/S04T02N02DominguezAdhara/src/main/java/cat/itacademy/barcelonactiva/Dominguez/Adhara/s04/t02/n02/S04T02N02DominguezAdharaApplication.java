e06b69243f5d4f90178c87e99cbf38d0b3569cc0
Adhara Domínguez <98258406+draadhara@users.noreply.github.com>
2022-06-17 08:07:59 +0200

    S402 N2 Initial commit

---
+++ S4/S04T02N02DominguezAdhara/src/main/java/cat/itacademy/barcelonactiva/Dominguez/Adhara/s04/t02/n02/S04T02N02DominguezAdharaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Dominguez.Adhara.s04.t02.n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N02DominguezAdharaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N02DominguezAdharaApplication.class, args);
+	}
+
+}
